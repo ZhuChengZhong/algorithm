935cf4d5bd0b1cad67388b0cb7cc22867f0d1cb7
ZhuChengZhong <956281507@qq.com>
2018-08-26 12:09:01 +0800

    merge two sorted lists

---
+++ LeetCode/MergeTwoLists.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class MergeTwoLists {
+	static class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode(int x) {
+		          val = x;
+		          next = null;
+		      }
+		  }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        ListNode empty=new ListNode(-1);
+        ListNode p=empty;
+        while(l1!=null&&l2!=null){
+            if(l1.val<l2.val){
+                p.next=l1;
+                l1=l1.next;
+            }else{
+                p.next=l2;
+                l2=l2.next;
+            }
+            p=p.next;
+        }
+        if(l1!=null) p.next=l1;
+        if(l2!=null) p.next=l2;
+        return empty.next;
+    }
+}
