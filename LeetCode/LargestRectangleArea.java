c6ab1cc915a80bc0706dfe54c7130175a1e9df55
ZhuChengZhong <956281507@qq.com>
2018-08-28 11:10:10 +0800

    largest rectangle in histogram

--- LeetCode/LargestRectangleArea.java
+++ LeetCode/LargestRectangleArea.java
@@ -3,32 +3,30 @@ package leetcode;
 import java.util.Stack;
 
 public class LargestRectangleArea {
-	   public int largestRectangleArea(int[] height) {
-	        if(height==null||height.length==0){
-	            return 0;
-	        }
-	        int max=0;
-	        Stack<Integer>stack=new Stack<>();
-	        for(int i=0;i<height.length;i++){
-	            int width=0;
-	            while(!stack.isEmpty()&&stack.peek()>height[i]){
-	                width++;
-	                int h=stack.pop();
-	                int area=h*width;
-	                max=max>area?max:area;
-	            }
-	            while(width>=0){
-	                stack.push(height[i]);
-	                width--;
-	            }
-	        }
-	        int width=0;
-	        while(!stack.isEmpty()){
-	            width++;
-	            int h=stack.pop();
-	            int area=h*width;
-	            max=max>area?max:area;
-	        }
-	        return max;
-	    }
+    public static int largestRectangleArea(int[] height) {
+        if(height==null||height.length==0){
+            return 0;
+        }
+        int max=0;
+        Stack<Integer>stack=new Stack<>();
+        stack.push(-1);
+        for(int i=0;i<height.length;i++){
+            while(stack.peek()!=-1&&height[stack.peek()]>height[i]){
+                int h=stack.pop();
+                int area=(i-1-stack.peek())*height[h];
+                max=max>area?max:area;
+            }
+            stack.push(i);
+        }
+        while(stack.peek()!=-1){
+            int h=stack.pop();
+             int area=(height.length-1-stack.peek())*height[h];
+            max=max>area?max:area;
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+		int[]height={0};
+		System.out.println(largestRectangleArea(height));
+	}
 }
