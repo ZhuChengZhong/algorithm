60337f5004cc89d47385d90e564852105699c67f
ZhuChengZhong <956281507@qq.com>
2018-08-31 17:04:25 +0800

    unique binary search trees

---
+++ LeetCode/NumTrees.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class NumTrees {
+    public int numTrees(int n) {
+        int[]dp=new int[n+1];
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                dp[i]+=dp[j]*dp[i-1-j];
+            }
+        }
+        return dp[n];
+    }
+}
