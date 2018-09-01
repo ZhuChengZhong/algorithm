155cb49d96ed1827a486b8129e25196fefc1edf5
ZhuChengZhong <956281507@qq.com>
2018-09-01 10:58:27 +0800

    maximum depth of binary tree

---
+++ LeetCode/MaxDepth.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class MaxDepth {
+	static class TreeNode {
+		      int val;
+		      TreeNode left;
+		      TreeNode right;
+		      TreeNode(int x) { val = x; }
+		  }
+	   public int maxDepth(TreeNode root) {
+	        if(root==null) return 0;
+	        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+	    }
+}
